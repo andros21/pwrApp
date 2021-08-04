ffd062732502917a2722dc905c2a1b474e79d5d2
Andrea Rossoni <andrea.ros21@mail.com>
2021-08-04 21:08:15 +0200

    pwrApp: changes for heroku deploy
    * add `Procfile`
    * no multi year logic for heroku deploy
      (not enough postgresql space :sad:)
    * add `runtime.txt` with python version

--- pwrApp.py
+++ pwrApp.py
@@ -19,15 +19,13 @@
 import calendar
 import datetime
 import os
-import re
-import sqlite3
 
 import dash
-from dash import dcc
-from dash import html
-from dash import dash_table as dtt
 import numpy as np
 import pandas as pd
+import sqlalchemy
+from dash import dash_table as dtt
+from dash import dcc, html
 from dash.dependencies import Input, Output
 from flask import Flask
 
@@ -39,6 +37,7 @@ app.title = "ePwr Stats"
 
 # Default vars
 # -------------
+db_uri = os.environ["DATABASE_URL"].replace("postgres", "postgresql")
 dyear = int((datetime.datetime.now() - datetime.timedelta(days=1)).year)
 dmonth = 1
 dday = 1
@@ -58,13 +57,13 @@ def serve_layout():
         footer = fs.read()
         footer = footer.replace("YEAR", str(datetime.datetime.now().year))
 
-    # Define daily stats table, and search for
-    # different year DBs
+    # Load daily stats table from local database
     # ----------------------------------------------
-    yearDBs = [
-        re.search(r"20[2-9][0-9]", os.path.basename(db)).group()
-        for db in os.listdir(os.path.dirname(f"{os.environ['DATABASE_URL']}.{dyear}"))
-    ]
+    engine = sqlalchemy.create_engine(db_uri)
+    dailyStats = pd.read_sql(
+        "select * from avg", engine, index_col="index", parse_dates=["index"]
+    )
+    engine.dispose()
 
     # Build app layout and return it
     # ----------------------------------------------
@@ -74,18 +73,25 @@ def serve_layout():
             dcc.Markdown(header, className="header-div"),
             html.Div(
                 [
-                    dcc.Slider(
-                        id="year-slider",
-                        className="month-slider",
-                        min=int(min(yearDBs)),
-                        max=int(max(yearDBs)),
-                        value=dyear,
-                        marks={int(year): year for year in yearDBs},
-                        step=None,
+                    html.Div(
+                        id="serialize-df",
+                        style={"display": "none"},
+                        children=dailyStats.to_json(),
                     ),
-                    html.Div(id="serialize-df", style={"display": "none"}),
                     dcc.Slider(
-                        id="month-slider", min=1, max=12, className="month-slider"
+                        id="month-slider",
+                        className="month-slider",
+                        min=dailyStats.index.month.min(),
+                        max=dailyStats.index.month.max(),
+                        value=dailyStats.index.month.min(),
+                        marks={
+                            month: datetime.date(1900, month, 1).strftime("%b")
+                            for month in range(
+                                dailyStats.index.month.min(),
+                                dailyStats.index.month.max() + 1,
+                            )
+                        },
+                        step=None,
                     ),
                     dcc.Graph(id="hist-slider", className="hist-slider"),
                     html.Div(
@@ -156,67 +162,16 @@ app.layout = serve_layout
 
 
 @app.callback(
-    Output("serialize-df", "children"),
-    Input("year-slider", "value"),
-)
-def update_serializer(year):
-    db_uri = f"{os.environ['DATABASE_URL']}.{year}" if year is not None else dyear
-    engine = sqlite3.connect(db_uri)
-    dailyStats = pd.read_sql(
-        "select * from avg", engine, index_col="index", parse_dates=["index"]
-    )
-    engine.close()
-    return dailyStats.to_json()
-
-
-@app.callback(
-    [
-        Output("month-slider", "min"),
-        Output("month-slider", "max"),
-        Output("month-slider", "value"),
-        Output("month-slider", "marks"),
-        Output("month-slider", "step"),
-    ],
-    Input("serialize-df", "children"),
-)
-def update_month(ser):
-    """
-    Update month slider as function of
-    selected database year.
-    In case of None use Current year as default year
-    """
-    dailyStats = pd.read_json(ser)
-    return (
-        dailyStats.index.month.min(),
-        dailyStats.index.month.max(),
-        dailyStats.index.month.min(),
-        {
-            month: datetime.date(1900, month, 1).strftime("%b")
-            for month in range(
-                dailyStats.index.month.min(), dailyStats.index.month.max() + 1
-            )
-        },
-        None,
-    )
-
-
-@app.callback(
     Output("hist-slider", "figure"),
-    [
-        Input("year-slider", "value"),
-        Input("month-slider", "value"),
-        Input("serialize-df", "children"),
-    ],
+    [Input("serialize-df", "children"), Input("month-slider", "value")],
 )
-def update_hist(year, month, ser):
+def update_hist(ser, month):
     """
     Update histogram plot as a function of
-    selected year in year-slider
     selected month in month-slider.
-    In case of None use Current year as default year
     In case of None use January as default month
     """
-    year = year if year is not None else dyear
+    year = dyear
     month = month if month is not None else dmonth
     dailyStats = pd.read_json(ser)
     df = dailyStats.loc[f"{year}-{str(month).zfill(2)}"]
@@ -246,20 +201,15 @@ def update_hist(year, month, ser):
     }
 
 
-@app.callback(
-    Output("mark-slider", "children"),
-    [Input("year-slider", "value"), Input("month-slider", "value")],
-)
-def update_mark(year, month):
+@app.callback(Output("mark-slider", "children"), [Input("month-slider", "value")])
+def update_mark(month):
     """
     Update the markdown description next to
     the daily history plot, as function of
-    selected year in year-slider
     selected month in month-slider.
-    In case of None use Current year as default year
     In case of None use January as default month
     """
-    year = year if year is not None else dyear
+    year = dyear
     month = month if month is not None else dmonth
     des1 = (
         "**Table**: stats for the "
@@ -273,18 +223,15 @@ def update_mark(year, month):
 
 @app.callback(
     Output("mark1-slider", "children"),
-    [
-        Input("year-slider", "value"),
-        Input("serialize-df", "children"),
-    ],
+    [Input("serialize-df", "children")],
 )
-def update_mark1(year, ser):
+def update_mark1(ser):
     """
     Update the markdown description next to
     the year pie plot.
     In case of None use January as default month
     """
-    year = year if year is not None else dyear
+    year = dyear
     dailyStats = pd.read_json(ser)
     v = np.around(dailyStats.sum(), 2)[0]
     des1 = "**Pie**: Percentuage " + f"of kWh for all months avaiable inside {year}"
@@ -295,22 +242,16 @@ def update_mark1(year, ser):
 
 @app.callback(
     Output("mtable-slider", "data"),
-    [
-        Input("year-slider", "value"),
-        Input("month-slider", "value"),
-        Input("serialize-df", "children"),
-    ],
+    [Input("serialize-df", "children"), Input("month-slider", "value")],
 )
-def update_mtable(year, month, ser):
+def update_mtable(ser, month):
     """
     Update the stats table next to the year
-    pie plot, as function of
-    selected year in year-slider
-    selected month in month-slider.
-    In case of None use Current year as default year
+    pie plot, as function of selected month in
+    month-slider.
     In case of None use January as default month
     """
-    year = year if year is not None else dyear
+    year = dyear
     month = month if month is not None else dmonth
     dailyStats = pd.read_json(ser)
     df = dailyStats.loc[f"{year}-{str(month).zfill(2)}"]["kWh"]
@@ -325,22 +266,19 @@ def update_mtable(year, month, ser):
     Output("plot-hover", "figure"),
     [
         Input("serialize-df", "children"),
-        Input("year-slider", "value"),
         Input("month-slider", "value"),
         Input("hist-slider", "clickData"),
     ],
 )
-def update_plot_hover(ser, year, month, hoverdict):
+def update_plot_hover(ser, month, hoverdict):
     """
     Update the history plot, as function of:
-      * selected year in year-slider
       * selected month in month-slider
       * selected day in hist-slider
-    In case of None use Current year as default year
     In case of None month use January as default
     In case of None day use 1st as default
     """
-    year = year if year is not None else dyear
+    year = dyear
     month = month if month is not None else dmonth
     dailyStats = pd.read_json(ser)
     df = dailyStats.loc[f"{year}-{str(month).zfill(2)}"]
@@ -355,12 +293,11 @@ def update_plot_hover(ser, year, month, hoverdict):
         day = str(dday).zfill(2)
 
     table = f"{year}-{str(month).zfill(2)}-{day}"
-    db_uri = f"{os.environ['DATABASE_URL']}.{year}" if year is not None else dyear
-    engine = sqlite3.connect(db_uri)
+    engine = sqlalchemy.create_engine(db_uri)
     dailyHistory = pd.read_sql(
         f'select * from "{table}"', engine, index_col="index", parse_dates=["index"]
     )
-    engine.close()
+    engine.dispose()
 
     return {
         "data": [
@@ -386,17 +323,10 @@ def update_plot_hover(ser, year, month, hoverdict):
     }
 
 
-@app.callback(
-    Output("pie-slider", "figure"),
-    [Input("year-slider", "value"), Input("serialize-df", "children")],
-)
-def update_pie(year, ser):
-    """
-    Update the pie plot using dailyStats, as
-    function of selected year in year-slider
-    In case of None use Current year as default year
-    """
-    year = year if year is not None else dyear
+@app.callback(Output("pie-slider", "figure"), [Input("serialize-df", "children")])
+def update_pie(ser):
+    """Update the pie plot using dailyStats"""
+    year = dyear
     dailyStats = pd.read_json(ser)
     min_month = dailyStats.index.month.min()
     max_month = dailyStats.index.month.max()
