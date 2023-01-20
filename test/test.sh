8df1379c076fd74c03f174e866bac5b7aa7831a9
andros21 <andrea.ros21@e.email>
2023-01-20 09:27:01 +0100

    build(ci-cd.yml): add smoke test for pwrapp

---
+++ test/test.sh
@@ -0,0 +1,49 @@
+#!/usr/bin/env bash
+#
+# smoke test pwrapp with postgres
+# instance with partial db loaded
+
+set -o errexit -o nounset -o errtrace -o pipefail
+
+# create tuntap network
+docker network create --subnet 10.89.0.0/24 tun-tap
+
+# start postgres emulator
+mkdir -p "$PWD/test/data"
+docker run --rm \
+   --detach \
+   --interactive \
+   --network tun-tap \
+   --ip 10.89.0.2 \
+   --env POSTGRES_PASSWORD=password \
+   --name postgres_emulator \
+   --volume "$PWD/test/pwrapp.sql":/home/postgres/pwrapp.sql:Z \
+   --volume "$PWD/test/data":/var/lib/postgresql/data:Z \
+   cgr.dev/chainguard/postgres:latest
+
+# provising partial pwrapp db
+sleep 3
+docker exec -u postgres postgres_emulator sh -c 'createdb pwrapp'
+docker exec -u postgres postgres_emulator sh -c 'psql pwrapp < pwrapp.sql'
+
+# reconfigure postgres service
+# listen all addresses
+docker exec -u postgres postgres_emulator \
+   sh -c "echo listen_addresses = \'*\' >> /var/lib/postgresql/data/postgresql.conf"
+docker restart postgres_emulator
+
+# start and test pwrapp
+sleep 3
+docker run --rm \
+   --detach \
+   --network tun-tap \
+   --env DATABASE_URL=postgres://postgres:password@10.89.0.2:5432/pwrapp \
+   --name smoke_pwrapp \
+   -p 8080:8080 \
+   "${{ env.REGISTRY }}/${{ env.IMAGE_NAME }}@${{ steps.docker.outputs.digest }}"
+sleep 3
+curl -X GET http://localhost:8080
+docker logs smoke_pwrapp
+docker stop smoke_pwrapp
+docker stop postgres_emulator
+docker network rm tun-tap
