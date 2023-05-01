c715b218eb18bc7f7103359d0f30e4df98e2c233
andros21 <andrea.ros21@murena.io>
2023-05-01 11:49:44 +0200

    test(test.sh): log and exit 1 if curl fail

--- test/test.sh
+++ test/test.sh
@@ -12,15 +12,15 @@ docker network create --subnet 10.89.0.0/24 tun-tap
 # start postgres emulator
 mkdir -p "$PWD/test/data"
 docker run --rm \
-   --detach \
-   --interactive \
-   --network tun-tap \
-   --ip 10.89.0.2 \
-   --env POSTGRES_PASSWORD=password \
-   --name postgres_emulator \
-   --volume "$PWD/test/pwrapp.sql":/home/postgres/pwrapp.sql:Z \
-   --volume "$PWD/test/data":/var/lib/postgresql/data:Z \
-   cgr.dev/chainguard/postgres:latest
+    --detach \
+    --interactive \
+    --network tun-tap \
+    --ip 10.89.0.2 \
+    --env POSTGRES_PASSWORD=password \
+    --name postgres_emulator \
+    --volume "$PWD/test/pwrapp.sql":/home/postgres/pwrapp.sql:Z \
+    --volume "$PWD/test/data":/var/lib/postgresql/data:Z \
+    cgr.dev/chainguard/postgres:latest
 
 # provising partial pwrapp db
 sleep 3
@@ -30,20 +30,20 @@ docker exec -u postgres postgres_emulator sh -c 'psql pwrapp < pwrapp.sql'
 # reconfigure postgres service
 # listen all addresses
 docker exec -u postgres postgres_emulator \
-   sh -c "echo listen_addresses = \'*\' >> /var/lib/postgresql/data/postgresql.conf"
+    sh -c "echo listen_addresses = \'*\' >> /var/lib/postgresql/data/postgresql.conf"
 docker restart postgres_emulator
 
 # start and test pwrapp
 sleep 3
 docker run \
-   --detach \
-   --network tun-tap \
-   --env DATABASE_URL="postgres://postgres:password@10.89.0.2:5432/pwrapp" \
-   --name smoke_pwrapp \
-   -p 8080:8080 \
-   "${PWRAPP_DEVEL}"
+    --detach \
+    --network tun-tap \
+    --env DATABASE_URL="postgres://postgres:password@10.89.0.2:5432/pwrapp" \
+    --name smoke_pwrapp \
+    -p 8080:8080 \
+    "${PWRAPP_DEVEL}"
 sleep 3
-curl -X GET http://localhost:8080 || docker logs smoke_pwrapp
+curl -X GET http://localhost:8080 || (docker logs smoke_pwrapp && exit 1)
 docker logs smoke_pwrapp
 docker stop smoke_pwrapp
 docker stop postgres_emulator
